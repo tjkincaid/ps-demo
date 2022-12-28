2621be5c0e2588bf6e5bcaad47b73851bafddd66
tjkin <tjkincaid92@gmail.com>
2022-12-28 13:05:43 -0500

    data binding.

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {UserSettings} from "../data/user-settings";
 
 @Component({
   selector: 'app-user-settings-form',
@@ -6,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-settings-form.component.css']
 })
 export class UserSettingsFormComponent implements OnInit {
-
+  OriginalUserSettings: UserSettings={
+    name: 'TJ',
+    emailOffers: true,
+    interfaceStyle: 'dark',
+    subscriptionType: 'Annual',
+    notes: 'TJ has some notes...'
+  }
+  userSettings:UserSettings={...this.OriginalUserSettings};
   constructor() { }
 
   ngOnInit(): void {
